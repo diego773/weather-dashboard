a81e7a613704a57816db35a170eb60875b98b301
Diego <diegovillarreal123@gmail.com>
2021-03-11 15:09:20 -0600

    i keep getting undefined error messages

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ var apiKey = "0e40e7fc7657400e150779275327d76e";
 
 
 
-
+// Creating a search weather value
 function getSearchValue() {
     var searchValue = document.getElementById("search-value").value;
 
     searchWeather(searchValue);
 };
 
-
+// Creating a function were it shows the result in temperature from that city
 function searchWeather(searchValue) {
     console.log(searchValue);
+
+    // Getting the information from weather API and converting them in imperial value
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + apiKey)
         .then(function (response) {
             return response.json();
@@ -25,14 +27,15 @@ function searchWeather(searchValue) {
             console.log(data);
         var todayEl = document.getElementById("today");
         todayEl.textContent = "";
-
+        // 
         var titleEl = document.createElement("h3");
         titleEl.classList.add("card-title");
         titleEl.textContent = data.name + " (" + new Date().toLocaleDateString() + ")";
-
+        
         var cardEl = document.createElement("div");
         cardEl.classList.add("card");
 
+        // Getting data from the console
         var temperatureEl = document.createElement("p");
         temperatureEl.textContent = "Temperature: " + data.main.temp + "℉";
         temperatureEl.classList.add("card-text");
@@ -42,24 +45,34 @@ function searchWeather(searchValue) {
         humidityEl.classList.add("card-text");
 
         var windEl = document.createElement("p");
-        windEl.textContent = "Wind: " + data.wind.speed + "mph";
+        windEl.textContent = "Wind; " + data.wind.speed + "mph";
         windEl.classList.add("card-text");
-        
-        var uvIndexEl = document.createElement("p");
-        uvIndexEl.textContent = "UV Index"
-        uvIndexEl.classList.add("card-text");
-        
-        fetch("http://api.openweathermap.org/data/2.5/uvi?" + "lat={lat}&lon={lon}" &appid + apiKey)
 
         var cardBodyEl = document.createElement("div");
         cardBodyEl.classList.add("card-body");
 
-
+        // Shows img of the weather on that day
         var imageEl = document.createElement("img");
         imageEl.setAttribute("src", " http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
 
-        titleEl.appendChild(imageEl);
+        // getUvIndex(data.coord.lat, data.coord.lon);
+        var uvIndex = document.createElement("p");
+        uvIndex.textContent = "UV Index: " + ("http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + lon + "appid"); 
 
+        var latitudeEl = document.createElement("lat");
+        latitudeEl.textContent = "Latitude" + data.coord.lat + "appid";
+        latitudeEl.classList.add("card-text");
+
+        var logitudeEl = document.createElement("lat");
+        logitudeEl.textContent = "Latitude" + data.coord.lon + "appid";
+        logitudeEl.classList.add("card-text");
+        // getForecast(searchValue);
+
+
+            
+        
+        titleEl.appendChild(imageEl);
+        
         cardBodyEl.appendChild(titleEl);
         
         cardBodyEl.appendChild(temperatureEl);
@@ -67,23 +80,22 @@ function searchWeather(searchValue) {
         cardBodyEl.appendChild(humidityEl);
 
         cardBodyEl.appendChild(windEl);
-
-        cardBodyEl.appendChild(uvIndexEl);
         
         cardEl.appendChild(cardBodyEl);
 
         todayEl.appendChild(cardEl);
-
-        // getForecast(searchValue);
         
-        // getUvIndex(data.coord.lat, data.coord.lon);
         
         // call for forecast use different api
         // call for uv index use different api
 
-    }   )
         
+    })  
+    
+    
 }
+    
+    searchButton.addEventListener("click", getSearchValue);
 
 
 
@@ -110,4 +122,3 @@ function searchWeather(searchValue) {
 
 
 
-searchButton.addEventListener("click", getSearchValue);
